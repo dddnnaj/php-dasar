dd8e5ae5a79dfb7feb945b71a1db28a5ab0476f0
dadan <kuden1234566@gmail.com>
2025-09-01 13:55:47 +0700

    tambah file latihan

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
     <title>php dasar</title>
 </head>
 <body>
-    <?php $nama= "dadan"?>
-    <h1>selamat datang<?php echo $nama?></h1>
+    <?php $nama= "dadan";
+          $umur= "16"
+          ?>
+    <h1>selamat datang <td><?php echo $nama?></td></h1>
+    <h1>umur<?php echo $umur?></h1>
 </body>
 </html>
+<?php
+$nilai = 75;
+
+if ($nilai  >= 75) {
+    echo "selamat, kamu lulus";
+}
---
+++ latihan.php
@@ -0,0 +1,10 @@
+<?php
+$nilai =90;
+$nilai =75;
+$nilai =60;
+
+if ($nilai  >= 90) {
+    echo "lulus dengan peringkat A";
+}
+
+
